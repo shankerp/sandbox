88d2f50f40fa3d1bad51712a8c335697346d754f
Bharathwaaj Srinivasan <bharathwaaj.s@gmail.com>
2011-06-22 15:24:18 +0530

    Updated check environment variable.

--- bash/check_environment_variable.sh
+++ bash/check_environment_variable.sh
@@ -2,5 +2,11 @@ if [ -z "$CROSS_C_ROOT_PATH" ]; then
 echo "Need to set CROSS_C_ROOT_PATH"
 fi
 
+if ! [ -z "$CROSS_C_ROOT_PATH" ]; then
+echo "Already set CROSS_C_ROOT_PATH"
+fi
+
 ${STATE?"Need to set STATE"}
 ${DEST:?"Need to set DEST non-empty"}
+end:
+;
