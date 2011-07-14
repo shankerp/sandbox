d4c6913c84cfacfb6a69bf01c8694474198b2161
Bharathwaaj Srinivasan <bharathwaaj.s@gmail.com>
2011-07-14 21:13:00 +0530

    Work in progress

---
+++ python/buildscript/unittest_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import unittest
+import model
+
+class TestModel(unittest.TestCase):
+    """
+        A test class for model module.
+    """
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
